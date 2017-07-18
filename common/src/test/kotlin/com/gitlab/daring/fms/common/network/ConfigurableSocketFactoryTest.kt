b979d38384d02dbe0dc50e262457b9dd720edcd4
daring <danila.daring@gmail.com>
2017-07-18 17:30:33 +0300

    issue #2 - extend ConfigurableSocketFactoryTest

--- common/src/test/kotlin/com/gitlab/daring/fms/common/network/ConfigurableSocketFactoryTest.kt
+++ common/src/test/kotlin/com/gitlab/daring/fms/common/network/ConfigurableSocketFactoryTest.kt
@@ -2,6 +2,7 @@ package com.gitlab.daring.fms.common.network
 
 import org.junit.Test
 import org.mockito.Mockito.*
+import java.net.InetAddress
 import java.net.InetSocketAddress
 import java.net.Socket
 
@@ -12,14 +13,21 @@ class ConfigurableSocketFactoryTest {
         val tf = TestSocketFactory()
 
         val s1 = tf.createSocket("h1", 1)
-        verify(s1).connect(InetSocketAddress("h1", 1), 100)
         verify(s1).soTimeout = 101
+        verify(s1).connect(InetSocketAddress("h1", 1), 100)
         verifyNoMoreInteractions(s1)
 
         val s2 = tf.createSocket(null as String?, 1)
-        verify(s2).connect(tf.nullAddress(1), 100)
         verify(s2).soTimeout = 101
+        verify(s2).connect(tf.nullAddress(1), 100)
         verifyNoMoreInteractions(s2)
+
+        val lh =  InetAddress.getLocalHost()
+        val s3 = tf.createSocket("h1", 1, lh, 2)
+        verify(s3).soTimeout = 101
+        verify(s3).bind(InetSocketAddress(lh, 2))
+        verify(s3).connect(InetSocketAddress("h1", 1), 100)
+        verifyNoMoreInteractions(s3)
 
         //TODO extend
     }
