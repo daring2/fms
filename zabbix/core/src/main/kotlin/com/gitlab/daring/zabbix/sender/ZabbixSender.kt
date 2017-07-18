d1bf5a48c857a9805e06e6c3f1d1db87c071e847
daring <danila.daring@gmail.com>
2017-07-18 17:41:54 +0300

    issue #2 - zabbix-core - add dependency from common module

--- zabbix/core/src/main/kotlin/com/gitlab/daring/zabbix/sender/ZabbixSender.kt
+++ zabbix/core/src/main/kotlin/com/gitlab/daring/zabbix/sender/ZabbixSender.kt
@@ -1,5 +1,6 @@
 package com.gitlab.daring.zabbix.sender
 
+import com.typesafe.config.Config
 import javax.net.SocketFactory
 
 interface ZabbixSender {
@@ -11,5 +12,7 @@ class ZabbixSenderImpl(
         val port: Int = 10051,
         val socketFactory: SocketFactory = SocketFactory.getDefault()
 ): ZabbixSender {
-    
+
+    constructor(c: Config) : this(c.getString("host"), c.getInt("port"))
+
 }
