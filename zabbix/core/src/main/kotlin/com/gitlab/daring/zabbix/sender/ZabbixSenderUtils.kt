d0d676154fcfe6fc7ec9bf9c7c1b6f70e4f463e5
daring <danila.daring@gmail.com>
2017-07-24 15:17:14 +0300

    issue #2 - introduce ZabbixSenderUtils

---
+++ zabbix/core/src/main/kotlin/com/gitlab/daring/zabbix/sender/ZabbixSenderUtils.kt
@@ -0,0 +1,27 @@
+package com.gitlab.daring.zabbix.sender
+
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.node.ObjectNode
+import com.google.common.base.CharMatcher
+import java.time.Duration
+
+internal object ZabbixSenderUtils {
+
+    val ValueCharMatcher = CharMatcher.javaIsoControl().and(CharMatcher.whitespace().negate())
+
+    /**
+     * Removes unsupported chars from value string
+     */
+    fun normalizeValue(v: String): String {
+        return ValueCharMatcher.removeFrom(v)
+    }
+
+    /**
+     * Adds time fields (clock, ns) to given [JsonNode]
+     */
+    fun addTimeFields(n: ObjectNode, time: Duration, nanos: Int): Unit {
+        n.put("clock", time.seconds)
+        n.put("ns", time.nano + nanos)
+    }
+
+}
