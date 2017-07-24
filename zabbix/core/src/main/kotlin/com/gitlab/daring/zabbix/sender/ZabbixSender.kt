bda8039cf2106e222721bdce5b1989f7759ebf7c
daring <danila.daring@gmail.com>
2017-07-24 16:54:10 +0300

    issue #2 - implement ZabbixSender

--- zabbix/core/src/main/kotlin/com/gitlab/daring/zabbix/sender/ZabbixSender.kt
+++ zabbix/core/src/main/kotlin/com/gitlab/daring/zabbix/sender/ZabbixSender.kt
@@ -1,24 +1,35 @@
 package com.gitlab.daring.zabbix.sender
 
+import com.fasterxml.jackson.module.kotlin.readValue
+import com.gitlab.daring.fms.common.json.JsonUtils.JsonMapper
 import com.gitlab.daring.fms.common.network.SocketProvider
 import com.gitlab.daring.fms.common.network.SocketProviderImpl
+import com.gitlab.daring.zabbix.sender.ZabbixSenderUtils.HeaderSize
 import com.typesafe.config.Config
 
-interface ZabbixSender {
-
-    fun send(req: SendRequest): SendResult
-
-}
-
-class ZabbixSenderImpl(
+class ZabbixSender(
         val host: String,
         val port: Int = 10051,
         val socketProvider: SocketProvider = SocketProviderImpl(3000, 10000)
-): ZabbixSender {
+)  {
 
     constructor(c: Config) : this(c.getString("host"), c.getInt("port"))
 
-    override fun send(req: SendRequest): SendResult {
-        TODO("not implemented")
+    fun send(req: SendRequest): SendResult {
+        val socket = socketProvider.createSocket(host, port)
+        socket.use {
+            val rn = req.buildJson()
+            JsonMapper.writeValue(socket.getOutputStream(), rn)
+            val rbs = socket.getInputStream().readBytes()
+            return parseResult(rbs)
+        }
     }
+
+    private fun parseResult(bs: ByteArray): SendResult {
+        if (bs.size < HeaderSize)
+            throw RuntimeException("invalid result")
+        val str = String(bs, HeaderSize, bs.size - HeaderSize, Charsets.UTF_8)
+        return JsonMapper.readValue(str)
+    }
+
 }
