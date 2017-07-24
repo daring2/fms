bd6374fbe3eef3bce5ae908c826bea8212c6da5d
daring <danila.daring@gmail.com>
2017-07-24 16:53:48 +0300

    issue #2 - introduce SendRequest

--- zabbix/core/src/main/kotlin/com/gitlab/daring/zabbix/sender/SendRequest.kt
+++ zabbix/core/src/main/kotlin/com/gitlab/daring/zabbix/sender/SendRequest.kt
@@ -1,8 +1,30 @@
 package com.gitlab.daring.zabbix.sender
 
+import com.fasterxml.jackson.databind.node.ObjectNode
+import com.gitlab.daring.fms.common.json.JsonUtils.JsonMapper
+import com.gitlab.daring.zabbix.sender.ZabbixSenderUtils.addTimeFields
 import java.time.Duration
 
 data class SendRequest(
         val data: List<ItemValue>,
-        val time: Duration? = null
-)
+        val time: Duration? = null,
+        val proxy: String? = null
+) {
+
+    fun buildJson(): ObjectNode {
+        val n = JsonMapper.createObjectNode()
+        if (proxy != null) {
+            n.put("request", "history data")
+            n.put("host", proxy)
+        } else {
+            n.put("request", "sender data")
+        }
+        val dn = n.putArray("data")
+        data.forEachIndexed { i, v ->
+            dn.add(v.buildJson(i))
+        }
+        time?.let { addTimeFields(n, it, 0) }
+        return n
+    }
+
+}
---
+++ zabbix/core/src/test/kotlin/com/gitlab/daring/zabbix/sender/SendRequestTest.kt
@@ -0,0 +1,35 @@
+package com.gitlab.daring.zabbix.sender
+
+import com.fasterxml.jackson.databind.JsonNode
+import com.gitlab.daring.fms.common.json.JsonUtils.JsonMapper
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import java.time.Duration
+
+class SendRequestTest {
+
+    @Test
+    fun testBuildJson() {
+        val vs1 = listOf(
+                ItemValue("h1", "k1", "v1"),
+                ItemValue("h2", "k2", "v2", isError = true)
+        )
+        val req1 = SendRequest(vs1, Duration.ofMillis(1002))
+        val d1 =  "data: [" +
+                "{host: 'h1', key: 'k1', value: 'v1'}," +
+                "{host: 'h2', key: 'k2', value: 'v2', state: 1}" +
+                "]"
+        val n1 = "{request: 'sender data', $d1, clock: 1, ns: 2000000}"
+        assertJsonEquals(n1, req1.buildJson())
+
+        val req2 = req1.copy(proxy = "p1")
+        val n2 = "{request: 'history data', host: 'p1', $d1, clock: 1, ns: 2000000}"
+        assertJsonEquals(n2, req2.buildJson())
+    }
+
+    fun assertJsonEquals(exp: String, n: JsonNode) {
+        val expNode = JsonMapper.readTree(exp)
+        assertEquals("" + expNode, "" + n)
+    }
+
+}
