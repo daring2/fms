029064bc6602a3de2cfd16ae2b44283356b067b4
daring <danila.daring@gmail.com>
2017-07-24 16:24:37 +0300

    issue #2 - introduce ItemValue

--- zabbix/core/src/main/kotlin/com/gitlab/daring/zabbix/sender/ItemValue.kt
+++ zabbix/core/src/main/kotlin/com/gitlab/daring/zabbix/sender/ItemValue.kt
@@ -1,6 +1,10 @@
 package com.gitlab.daring.zabbix.sender
 
+import com.fasterxml.jackson.databind.node.ObjectNode
+import com.gitlab.daring.fms.common.json.JsonUtils.JsonMapper
+import com.gitlab.daring.zabbix.sender.ZabbixSenderUtils.addTimeFields
 import java.time.Duration
+import com.gitlab.daring.zabbix.sender.ZabbixSenderUtils.normalizeValue
 
 data class ItemValue(
         val host: String,
@@ -9,5 +13,15 @@ data class ItemValue(
         val time: Duration? = null,
         val isError: Boolean = false
 ) {
+
+    fun buildJson(i: Int) : ObjectNode {
+        val n = JsonMapper.createObjectNode()
+        n.put("host", host)
+        n.put("key", key)
+        n.put("value", normalizeValue(value))
+        if (isError) n.put("state", 1)
+        time?.let { addTimeFields(n, it, i) }
+        return n
+    }
 
 }
