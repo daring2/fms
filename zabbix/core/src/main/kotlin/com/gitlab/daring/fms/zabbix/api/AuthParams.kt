a5ce4bab428f859151b08f5ef6a5687340912825
daring <danila.daring@gmail.com>
2017-07-28 18:32:35 +0300

    issue #2 - refactoring

--- zabbix/core/src/main/kotlin/com/gitlab/daring/fms/zabbix/api/AuthParams.kt
+++ zabbix/core/src/main/kotlin/com/gitlab/daring/fms/zabbix/api/AuthParams.kt
@@ -5,7 +5,7 @@ import java.time.Duration
 data class AuthParams(
         val user: String,
         val password: String,
-        val tokenTimeout: Duration
+        val tokenTimeout: Duration = Duration.ofMinutes(10)
 ) {
 
     val apiParams = mapOf("user" to user, "password" to password)
