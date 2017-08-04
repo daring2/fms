787446d12774aed4bbe6bd238bcd32cbf3105acd
daring <danila.daring@gmail.com>
2017-08-04 21:59:13 +0300

    issue #2 - refactoring

--- common/src/main/kotlin/com/gitlab/daring/fms/common/concurrent/ConcurrentUtils.kt
+++ common/src/main/kotlin/com/gitlab/daring/fms/common/concurrent/ConcurrentUtils.kt
@@ -1,18 +1,20 @@
 package com.gitlab.daring.fms.common.concurrent
 
+import com.gitlab.daring.fms.common.config.getMillis
+import com.gitlab.daring.fms.common.config.getOpt
 import com.typesafe.config.Config
 import java.util.concurrent.LinkedBlockingQueue
 import java.util.concurrent.ThreadPoolExecutor
-import java.util.concurrent.TimeUnit.SECONDS
+import java.util.concurrent.TimeUnit.MILLISECONDS
 
 object ConcurrentUtils {
 
     fun newExecutor(c: Config): ThreadPoolExecutor {
         val size = c.getInt("size")
-        //TODO support all params
+        val maxSize = c.getOpt { getInt("maxSize") } ?: size
+        val keepAlive = c.getOpt { getMillis("keepAlive") } ?: 60000
         return ThreadPoolExecutor(
-                size, size,
-                60L, SECONDS,
+                size, maxSize, keepAlive, MILLISECONDS,
                 LinkedBlockingQueue<Runnable>()
         )
     }
--- zabbix/core/src/test/kotlin/com/gitlab/daring/fms/common/concurrent/ConcurrentUtilsTest.kt
+++ zabbix/core/src/test/kotlin/com/gitlab/daring/fms/common/concurrent/ConcurrentUtilsTest.kt
@@ -4,14 +4,20 @@ import com.gitlab.daring.fms.common.concurrent.ConcurrentUtils.newExecutor
 import com.gitlab.daring.fms.common.config.ConfigUtils.configFromString
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import java.util.concurrent.TimeUnit.MILLISECONDS
 
 class ConcurrentUtilsTest {
 
     @Test
     fun testNewExecutor() {
-        val exec = newExecutor(configFromString("{size = 5}"))
-        assertEquals(5, exec.corePoolSize)
-        assertEquals(5, exec.maximumPoolSize)
+        val e1 = newExecutor(configFromString("{ size = 5 }"))
+        assertEquals(5, e1.corePoolSize)
+        assertEquals(5, e1.maximumPoolSize)
+        assertEquals(60000, e1.getKeepAliveTime(MILLISECONDS))
+        val e2 = newExecutor(configFromString("{ size = 1, maxSize=2, keepAlive=3s }"))
+        assertEquals(1, e2.corePoolSize)
+        assertEquals(2, e2.maximumPoolSize)
+        assertEquals(3000, e2.getKeepAliveTime(MILLISECONDS))
     }
 
 }
