62c7d81cff163a68f18067e859d407725b2abe74
daring <danila.daring@gmail.com>
2017-08-13 21:08:19 +0300

    issue #2 - implement AgentPassiveServerTest

--- zabbix/core/src/main/kotlin/com/gitlab/daring/fms/zabbix/agent/passive/AgentPassiveServer.kt
+++ zabbix/core/src/main/kotlin/com/gitlab/daring/fms/zabbix/agent/passive/AgentPassiveServer.kt
@@ -1,11 +1,14 @@
 package com.gitlab.daring.fms.zabbix.agent.passive
 
+import com.gitlab.daring.fms.common.concurrent.ConcurrentUtils.newExecutor
+import com.gitlab.daring.fms.common.config.getMillis
 import com.gitlab.daring.fms.zabbix.model.ItemValue
 import com.gitlab.daring.fms.zabbix.util.ZabbixProtocolUtils.HeaderSize
 import com.gitlab.daring.fms.zabbix.util.ZabbixProtocolUtils.ZbxError
 import com.gitlab.daring.fms.zabbix.util.ZabbixProtocolUtils.ZbxNotSupported
 import com.gitlab.daring.fms.zabbix.util.ZabbixProtocolUtils.buildMessage
 import com.gitlab.daring.fms.zabbix.util.ZabbixSocketServer
+import com.typesafe.config.Config
 import java.net.Socket
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
@@ -21,6 +24,12 @@ class AgentPassiveServer(
     var valueSupplier: (String) -> ItemValue = { key ->
         ItemValue("", key, "Unsupported item key", state = 1)
     }
+
+    constructor(c: Config) : this(
+            c.getInt("port"),
+            c.getMillis("readTimeout").toInt(),
+            newExecutor(c.getConfig("executor"))
+    )
 
     override fun process(socket: Socket) {
         val line = socket.getInputStream().bufferedReader().readLine()
--- zabbix/core/src/test/kotlin/com/gitlab/daring/fms/zabbix/agent/passive/AgentPassiveServerTest.kt
+++ zabbix/core/src/test/kotlin/com/gitlab/daring/fms/zabbix/agent/passive/AgentPassiveServerTest.kt
@@ -1,9 +1,71 @@
 package com.gitlab.daring.fms.zabbix.agent.passive
 
+import com.gitlab.daring.fms.common.config.ConfigUtils.configFromString
+import com.gitlab.daring.fms.common.util.closeQuietly
+import com.gitlab.daring.fms.zabbix.model.ItemValue
+import com.gitlab.daring.fms.zabbix.util.MockSocketProvider
+import com.gitlab.daring.fms.zabbix.util.ZabbixProtocolUtils.ZbxError
+import com.gitlab.daring.fms.zabbix.util.ZabbixProtocolUtils.ZbxNotSupported
+import com.gitlab.daring.fms.zabbix.util.ZabbixProtocolUtils.buildMessage
+import io.kotlintest.matchers.shouldBe
 import io.kotlintest.specs.FunSpec
+import org.junit.Assert.assertArrayEquals
+import org.mockito.Mockito
+import java.util.concurrent.ThreadPoolExecutor
 
-class AgentPassiveServerTest : FunSpec({
+class AgentPassiveServerTest : FunSpec() {
 
-    //TODO implement
+    init {
 
-})
+        test("init") {
+            val c = configFromString("{ port=1, readTimeout=2s,  executor { size=3, maxSize=4 }}")
+            val cl = AgentPassiveServer(c)
+            cl.port shouldBe 1
+            cl.readTimeout shouldBe 2000
+            val exec = cl.executor as ThreadPoolExecutor
+            exec.corePoolSize shouldBe 3
+            exec.maximumPoolSize shouldBe 4
+        }
+
+        testWithContext("process") {
+            srv.start()
+            checkProcess("i1", "i1.v")
+            checkProcess("err1", "${ZbxNotSupported}err1.v")
+            checkProcess("fail1", ZbxError)
+        }
+    }
+
+    internal fun testWithContext(name: String, f: TestContext.() -> Unit) {
+        test(name) { TestContext().use(f) }
+    }
+
+    internal class TestContext : AutoCloseable {
+
+        val sp = MockSocketProvider()
+        val srv = AgentPassiveServer(10, 100)
+
+        init {
+            srv.socketProvider = sp.serverProvider
+            srv.valueSupplier = { key ->
+                if (key.contains("fail")) throw RuntimeException(key)
+                val state = if (key.contains("err")) 1 else 0
+                ItemValue("", key, "$key.v", state = state)
+            }
+        }
+
+        fun checkProcess(req: String, res: String) {
+            sp.newSocket()
+            sp.setInput(req)
+            sp.accept()
+            val expOut = buildMessage(res)
+            assertArrayEquals(expOut, sp.output().toByteArray())
+            Mockito.verify(sp.socket).close()
+        }
+
+        override fun close() {
+            srv.closeQuietly()
+            sp.closeQuietly()
+        }
+    }
+
+}
