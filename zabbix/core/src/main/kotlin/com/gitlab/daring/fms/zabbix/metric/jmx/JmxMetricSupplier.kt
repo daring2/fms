1b81d2feb7ee27134dabc458b6eef4cdacf72f98
daring <danila.daring@gmail.com>
2017-08-20 19:06:11 +0300

    issue #2 - introduce JmxMetricSupplier

---
+++ zabbix/core/src/main/kotlin/com/gitlab/daring/fms/zabbix/metric/jmx/JmxMetricSupplier.kt
@@ -0,0 +1,33 @@
+package com.gitlab.daring.fms.zabbix.metric.jmx
+
+import com.gitlab.daring.fms.zabbix.metric.ErrorMetricSupplier
+import com.gitlab.daring.fms.zabbix.metric.Metric
+import com.gitlab.daring.fms.zabbix.metric.MetricSupplier
+import com.gitlab.daring.fms.zabbix.metric.MetricUtils.newMetricValue
+import com.gitlab.daring.fms.zabbix.model.ItemValue
+import java.lang.management.ManagementFactory
+import javax.management.MBeanServerConnection
+import javax.management.ObjectName
+
+class JmxMetricSupplier(
+        val connectionSupplier: () -> JmxConnection = ManagementFactory::getPlatformMBeanServer
+) : MetricSupplier {
+
+    override fun getValue(metric: Metric): ItemValue {
+        //TODO implement jmx.invoke metric
+        return when (metric.name) {
+            "jmx.get" -> getAttribute(metric)
+            else -> ErrorMetricSupplier.getValue(metric)
+        }
+    }
+
+    private fun getAttribute(m: Metric) = newMetricValue(m) {
+        require(m.params.size == 2, { "metric requires 2 parameters" })
+        val con = connectionSupplier()
+        val mbn = ObjectName(m.params[0])
+        con.getAttribute(mbn, m.params[1])
+    }
+
+}
+
+typealias JmxConnection = MBeanServerConnection
---
+++ zabbix/core/src/test/kotlin/com/gitlab/daring/fms/zabbix/metric/jmx/JmxMetricSupplierTest.kt
@@ -0,0 +1,34 @@
+package com.gitlab.daring.fms.zabbix.metric.jmx
+
+import com.gitlab.daring.fms.zabbix.metric.Metric
+import com.gitlab.daring.fms.zabbix.model.Item
+import com.gitlab.daring.fms.zabbix.model.ItemValue
+import io.kotlintest.matchers.shouldBe
+import io.kotlintest.mock.`when`
+import io.kotlintest.mock.mock
+import io.kotlintest.specs.FunSpec
+import javax.management.MBeanServerConnection
+import javax.management.ObjectName
+
+class JmxMetricSupplierTest : FunSpec({
+
+    test("jmx.get") {
+        val con = mock<MBeanServerConnection>()
+        val sup = JmxMetricSupplier({ con })
+        val bn1 = "d1:name=b1"
+        `when`(con.getAttribute(ObjectName(bn1), "a1")).thenReturn("v1")
+        val err2 = RuntimeException("err2")
+        `when`(con.getAttribute(ObjectName(bn1), "a2")).thenThrow(err2)
+
+        val m1 = Metric("jmx.get", listOf(bn1, "a1"), Item("i1"))
+        sup.getValue(m1) shouldBe ItemValue("v1", false, m1.item)
+
+        val m2 = m1.copy(params = listOf(bn1, "a2"))
+        sup.getValue(m2) shouldBe ItemValue("$err2", true, m1.item)
+
+        val m3 = m1.copy(params = listOf(bn1))
+        val err3 = IllegalArgumentException("metric requires 2 parameters")
+        sup.getValue(m3) shouldBe ItemValue("$err3", true, m1.item)
+    }
+
+})
