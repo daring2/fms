47023734888d101e0586b82c6518bb47f0dfeac8
daring <danila.daring@gmail.com>
2017-08-23 16:39:20 +0300

    issue #2 - add doc

--- zabbix/core/src/main/kotlin/com/gitlab/daring/fms/zabbix/metric/ErrorMetricSupplier.kt
+++ zabbix/core/src/main/kotlin/com/gitlab/daring/fms/zabbix/metric/ErrorMetricSupplier.kt
@@ -2,6 +2,9 @@ package com.gitlab.daring.fms.zabbix.metric
 
 import com.gitlab.daring.fms.zabbix.model.ItemValue
 
+/**
+ * Implementation of [MetricSupplier] that returns error values for all metrics
+ */
 object ErrorMetricSupplier : MetricSupplier {
 
     override fun getCurrentValue(metric: Metric): ItemValue {
